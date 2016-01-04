aad23ba4105d71ecdc0d376c6b70a8adf1357baa
Matthias Cullmann <matthias.cullmann@baloise.ch>
2016-01-04 14:55:42 +0100

    basic setup

---
+++ src/main/java/com/github/origin/install/Install.java
@@ -0,0 +1,11 @@
+package com.github.origin.install;
+
+import java.io.IOException;
+
+public class Install {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		System.out.println("Installing orig.in");
+	}
+
+}

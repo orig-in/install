a23a65c259b13efb3e1ee494f780d471c0fd4c14
Matthias Cullmann <matthias.cullmann@gmail.com>
2017-11-19 15:54:58 +0100

    installbuilder > latest

--- install.sh
+++ install.sh
@@ -18,22 +18,27 @@ if [ ! -d "$ORIGIN_HOME" ]; then
 	echo creating $ORIGIN_HOME
 	mkdir $ORIGIN_HOME
 fi
-JAVA_DOWNLOAD=http://get.jenv.mvnsearch.org/download/java/java-1.8.0_60.zip
-JAVA_DOWNLOAD_BIN=unknown
-case "`uname`" in
-    Darwin*)
-		JAVA_DOWNLOAD=http://get.jenv.mvnsearch.org/download/java/java-1.9.0-ea-mac.zip
-		JAVA_DOWNLOAD_BIN=$ORIGIN_HOME/java/java-1.9.0-ea/Contents/Home/bin/java
-        ;;
-esac
-JAVA_BIN=java
-if ! $JAVA_BIN -version; then
-	echo "java executable not found ... installing"
-	wget -N $JAVA_DOWNLOAD -O $ORIGIN_HOME/java.zip
-	unzip $ORIGIN_HOME/java.zip -d $ORIGIN_HOME/java/
-	JAVA_BIN=$JAVA_DOWNLOAD_BIN
-	$JAVA_BIN -version
+
+if [ ! -d "$ORIGIN_HOME/java/latest" ]; then
+	echo "java executable not found, installing ..."
+	
+    #TODO : what about 32 bit linux?
+	JAVA_DOWNLOAD=http://installbuilder.bitrock.com/java/jdk1.8.0_144-linux-x64.zip
+	JAVA_FOLDER=jdk1.8.0_144-linux-x64/java-linux-x64
+	case "`uname`" in
+	    Darwin*)
+			JAVA_DOWNLOAD=http://installbuilder.bitrock.com/java/jdk1.8.0_144-osx.zip
+			JAVA_FOLDER=jdk1.8.0_144-osx/java-osx
+	        ;;
+	esac
+	mkdir $ORIGIN_HOME/download/
+	wget -N $JAVA_DOWNLOAD -O $ORIGIN_HOME/download/java.zip
+	unzip $ORIGIN_HOME/download/java.zip -d $ORIGIN_HOME/download/java/
+	mkdir /home/matthi/.orig.in/java/
+	mv $ORIGIN_HOME/download/java/$JAVA_FOLDER $ORIGIN_HOME/java/latest
+	rm -rf $ORIGIN_HOME/download
 fi
+
 wget -N http://orig-in.github.io/download/install.jar -O $ORIGIN_HOME/install.jar
-java -jar $ORIGIN_HOME/install.jar
+$ORIGIN_HOME/java/latest/bin/java -jar $ORIGIN_HOME/install.jar
 

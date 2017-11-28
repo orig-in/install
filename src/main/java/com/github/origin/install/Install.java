1428a6ea7f46eb69c969479fa351c5a7430c8952
Matthias Cullmann <matthias.cullmann@gmail.com>
2017-11-28 18:36:32 +0100

    respect orgin_home

--- src/main/java/com/github/origin/install/Install.java
+++ src/main/java/com/github/origin/install/Install.java
@@ -1,5 +1,6 @@
 package com.github.origin.install;
 
+import java.io.File;
 import java.io.IOException;
 
 import com.github.origin.OperatingSystem;
@@ -7,8 +8,13 @@ import com.github.origin.OperatingSystem;
 
 public class Install {
 	
+	public final static File ORIGIN_HOME = detectHome();
+	public final static File ORIGIN_JAR = new File(ORIGIN_HOME, "origin.jar");
+	
 	public static void main(String[] args) throws InterruptedException, IOException {
-		System.out.println("Installing orig.in");
+		
+		System.out.println("Installing orig.in to "+ORIGIN_HOME.getAbsolutePath() );
+		new File(ORIGIN_HOME, "origin-upgrade.jar").renameTo(ORIGIN_JAR);
 		
 		// detect java
 		//check internet connection
@@ -24,6 +30,12 @@ public class Install {
 		default:
 			System.out.println("ERROR : operating system not yet supported.");
 		}
+	}
+
+	private static File detectHome() {
+		String tmp = System.getenv("ORIGIN_HOME");
+		if(tmp == null) tmp = System.getProperty("user.home");
+		return new File(tmp, ".orig.in");
 	}
 
 }

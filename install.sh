65b722ed03a9580bfe27a370138f8b5ff1e95e88
Matthias Cullmann <matthias.cullmann@gmail.com>
2019-05-05 15:12:18 +0200

    switched to AdoptOpenJDK 12.0.1_12

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+function mkdirif {
+	if [ ! -d "$1" ]; then
+		echo creating $1
+		mkdir -p $1
+	fi
+}  
+
 echo '                                              '
 echo '              __               __             '
 echo '  ___   _ __ /\_\     __      /\_\    ___     '
@@ -8,34 +15,37 @@ echo ' \/___/  \/_/   \/_/\/___L\ \/_/ \/_/\/_/\/_/ '
 echo '                      /\____/                 '
 echo '                      \_/__/                  '
 echo ''
+if [[ "$OSTYPE" == linux* ]]; then
+	OS='linux'
+fi
+if [[ "$OSTYPE" == darwin* ]]; then
+	OS='mac'
+fi
+
 if [[ -z "${ORIGIN_HOME}" ]]; then
   ORIGIN_HOME=~/.orig.in
 else
   ORIGIN_HOME="${ORIGIN_HOME}"
 fi
-
-if [ ! -d "$ORIGIN_HOME" ]; then
-	echo creating $ORIGIN_HOME
-	mkdir $ORIGIN_HOME
+if [ -d "$ORIGIN_HOME" ]; then
+   echo $ORIGIN_HOME exists
 fi
+
+mkdirif $ORIGIN_HOME
+
 
 if [ ! -d "$ORIGIN_HOME/java/latest" ]; then
 	echo "java executable not found, installing ..."
+	JAVA_DOWNLOAD_FILE=OpenJDK12U-jdk_x64_${OS}_hotspot_12.0.1_12.tar.gz
+	JAVA_DOWNLOAD=https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.1%2B12/$JAVA_DOWNLOAD_FILE
+	JAVA_FOLDER=jdk-12.0.1+12
 	
-    #TODO : what about 32 bit linux?
-	JAVA_DOWNLOAD=http://installbuilder.bitrock.com/java/jdk1.8.0_144-linux-x64.zip
-	JAVA_FOLDER=jdk1.8.0_144-linux-x64/java-linux-x64
-	case "`uname`" in
-	    Darwin*)
-			JAVA_DOWNLOAD=http://installbuilder.bitrock.com/java/jdk1.8.0_144-osx.zip
-			JAVA_FOLDER=jdk1.8.0_144-osx/java-osx
-	        ;;
-	esac
-	mkdir $ORIGIN_HOME/download/
-	wget -N $JAVA_DOWNLOAD -O $ORIGIN_HOME/download/java.zip
-	unzip $ORIGIN_HOME/download/java.zip -d $ORIGIN_HOME/download/java/
-	mkdir /home/matthi/.orig.in/java/
-	mv $ORIGIN_HOME/download/java/$JAVA_FOLDER $ORIGIN_HOME/java/latest
+	mkdirif $ORIGIN_HOME/download/
+	wget -N $JAVA_DOWNLOAD -O $ORIGIN_HOME/download/$JAVA_DOWNLOAD_FILE
+	
+	mkdirif $ORIGIN_HOME/java
+	tar -xzf $ORIGIN_HOME/download/$JAVA_DOWNLOAD_FILE -C $ORIGIN_HOME/java
+	ln -s $ORIGIN_HOME/java/$JAVA_FOLDER $ORIGIN_HOME/java/latest
 	rm -rf $ORIGIN_HOME/download
 fi
 

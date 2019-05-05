ad8d6b3f04ce86ac890feb4488e6780e80252390
Matthias Cullmann <matthias.cullmann@gmail.com>
2019-05-05 15:19:46 +0200

    FIX detectHome

--- src/main/java/com/github/origin/install/Install.java
+++ src/main/java/com/github/origin/install/Install.java
@@ -1,24 +1,25 @@
 package com.github.origin.install;
 
+import static java.lang.System.getProperty;
+
 import java.io.File;
 import java.io.IOException;
 
 import com.github.origin.OperatingSystem;
 
-
 public class Install {
-	
+
 	public final static File ORIGIN_HOME = detectHome();
 	public final static File ORIGIN_JAR = new File(ORIGIN_HOME, "origin.jar");
-	
+
 	public static void main(String[] args) throws InterruptedException, IOException {
-		
-		System.out.println("Installing orig.in to "+ORIGIN_HOME.getAbsolutePath() );
+
+		System.out.println("Installing orig.in to " + ORIGIN_HOME.getAbsolutePath());
 		new File(ORIGIN_HOME, "origin-upgrade.jar").renameTo(ORIGIN_JAR);
-		
+
 		// detect java
-		//check internet connection
-		
+		// check internet connection
+
 		System.out.println(OperatingSystem.CURRENT + " detected.");
 		switch (OperatingSystem.CURRENT) {
 		case MAC:
@@ -33,9 +34,8 @@ public class Install {
 	}
 
 	private static File detectHome() {
-		String tmp = System.getenv("ORIGIN_HOME");
-		if(tmp == null) tmp = System.getProperty("user.home");
-		return new File(tmp, ".orig.in");
+		String envVar = System.getenv("ORIGIN_HOME");
+		return envVar == null ? new File(getProperty("user.home"), ".orig.in") : new File(envVar);
 	}
 
 }
